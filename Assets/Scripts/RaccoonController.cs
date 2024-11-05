845639fd876b020b62facbe11abfcb0737b90ff1
turkan <t.badalzade2@student.vu.nl>
2024-11-05 17:03:40 -0500

    working on raccoon movement

--- Assets/Scripts/RaccoonController.cs
+++ Assets/Scripts/RaccoonController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class RaccoonController : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("Insert Main Camera")]
+    private Transform mainCamera;
+    
+    [SerializeField]
     [Tooltip("Insert Character Controller")]
     private CharacterController controller;
     
@@ -12,18 +16,32 @@ public class RaccoonController : MonoBehaviour
     [Tooltip("Insert Animator Controller")]
     private Animator playerAnimator;
     
+    public float speed = 2f;
+    
     // Start is called before the first frame update
-    private static readonly int IsThrowing = Animator.StringToHash("IsSwimming");
-    private static readonly int IsRunning = Animator.StringToHash("IsEating");
+    private static readonly int IsSwimming = Animator.StringToHash("IsSwimming");
+    private static readonly int IsEating = Animator.StringToHash("IsEating");
     private static readonly int IsWalking = Animator.StringToHash("IsWalking");
     void Start()
     {
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform playerTransform = transform;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 camForward = mainCamera.forward;
+        camForward.y = 0;
+        Vector3 camRight = mainCamera.right;
+        camRight.y = 0;
+        
+        Vector3 movement = (camForward * x) + (camRight * z);
+        movement.Normalize();
+        controller.Move(movement * (speed * Time.deltaTime));
         
     }
 }
