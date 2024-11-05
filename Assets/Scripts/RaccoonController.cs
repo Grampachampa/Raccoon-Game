da162925e6b2f71aae10275cb0bf315ed1974d7b
turkan <t.badalzade2@student.vu.nl>
2024-11-05 16:15:29 -0500

    Added AC_Raccoon_new

--- Assets/Scripts/RaccoonController.cs
+++ Assets/Scripts/RaccoonController.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class RaccoonController : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Insert Character Controller")]
+    private CharacterController controller;
+    
+    [SerializeField]
+    [Tooltip("Insert Animator Controller")]
+    private Animator playerAnimator;
+    
     // Start is called before the first frame update
+    private static readonly int IsThrowing = Animator.StringToHash("IsSwimming");
+    private static readonly int IsRunning = Animator.StringToHash("IsEating");
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
     void Start()
     {
         
