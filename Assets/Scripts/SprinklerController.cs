02b3ee099deab31b223d42be58894da1c57f60af
TeunBoekholt <teun.boekholt@gmail.com>
2024-11-08 15:47:05 -0500

    Sprinkler Cone Kinda works

--- Assets/Scripts/SprinklerController.cs
+++ Assets/Scripts/SprinklerController.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class SprinklerController : MonoBehaviour
 {
     public float rotationSpeed = 36.0f;
-    public GameObject sprinklerCone;
-    public Vector3 coneOffset = new Vector3(0, 0, 1);
-    private GameObject coneInstance;
     
     [SerializeField] 
     private ParticleSystem waterParticles;
@@ -17,7 +14,6 @@ public class SprinklerController : MonoBehaviour
     [SerializeField] private AudioClip[] sprinklerSounds;
     private AudioSource sprinklerAudio;
     
-
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -27,7 +23,6 @@ public class SprinklerController : MonoBehaviour
         sprinklerAudio.spatialBlend = 0f; 
         sprinklerAudio.maxDistance = 5.0f;
         
-        SpawnCone();
     }
 
     void Update()
@@ -36,43 +31,8 @@ public class SprinklerController : MonoBehaviour
         Transform sprinklerTransform = transform;
         
         // Rotate the sprinkler around the y-axis
-        sprinklerTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-        
-        // Checking if player is in spray
-        // if (inSprinkler(player, 45, 3))
-        // {
-        //     
-        // }
-
+        sprinklerTransform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
 
         // Spawning water
-    }
-
-    // bool inSprinkler(GameObject player,  float angle, float maxDistance)
-    // {
-    //     Vector3 heading = Vector3.Normalize(player.transform.position - transform.position);
-    //     float dot = Vector3.Dot(heading, transform.forward);
-    //     float cone = 1.0f - angle;
-    //     float distance =  (transform.position - player.transform.position).magnitude;
-    //     
-    //     if (dot >= cone && distance < maxDistance)
-    //     {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    
-    void SpawnCone()
-    {
-        // Instantiate the cone prefab and set it as a child of the sprinkler object
-        sprinklerCone = Instantiate(sprinklerCone, transform.position + transform.TransformDirection(coneOffset), transform.rotation);
-        coneInstance.transform.parent = transform; // Make the cone follow the sprinkler
-
-        // Ensure the cone is a trigger and invisible
-        var meshRenderer = coneInstance.GetComponent<MeshRenderer>();
-        if (meshRenderer) meshRenderer.enabled = false; // Hide the mesh renderer
-
-        var collider = coneInstance.GetComponent<MeshCollider>();
-        if (collider) collider.isTrigger = true;
     }
 }
