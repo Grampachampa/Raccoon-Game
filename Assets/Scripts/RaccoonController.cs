4145545d43e40685f2814301fee9787c03f54001
turkan <t.badalzade2@student.vu.nl>
2024-11-21 16:26:15 -0500

    messages for the game on level manager

--- Assets/Scripts/RaccoonController.cs
+++ Assets/Scripts/RaccoonController.cs
@@ -235,6 +235,7 @@ public class RaccoonController : MonoBehaviour
         if (other.gameObject.CompareTag("PotHole"))
         {
             //enter the new level
+            levelManager.enterNewLevel()
         }
         */
     }
